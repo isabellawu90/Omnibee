ddd52cd2513d7927f5c712093c5ab19a93d1d9b6
jrb485 <jrb485@cornell.edu>
2020-09-01 19:19:12 -0400

    updated restaurant model to include menus

--- Henfam/lib/models/restaurant.dart
+++ Henfam/lib/models/restaurant.dart
@@ -1,11 +1,14 @@
 import 'package:equatable/equatable.dart';
 
+import 'menu_item.dart';
+
 class Restaurant extends Equatable {
   final String name;
   final List<double> location;
   final String imagePath;
+  final List<MenuItem> menu;
 
-  Restaurant({this.name, this.location, this.imagePath});
+  Restaurant({this.name, this.location, this.imagePath, this.menu});
 
   @override
   List<Object> get props => [name, location, imagePath];
